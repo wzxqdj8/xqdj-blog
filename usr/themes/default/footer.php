1c50326331c6974f8d6d788eecebb62203dec7c5
小旗 <xqdj@protonmail.com>
2026-07-14 09:52:41 +0800

    Update footer.php

--- usr/themes/default/footer.php
+++ usr/themes/default/footer.php
@@ -1,29 +1,29 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 
-<footer id="rin-footer" style="text-align:center; padding:15px 0; font-size:14px; color:#666; line-height:1.3;">
-    <!-- 第一行：隐私政策、免责声明、站点地图、Robots -->
+<footer id="rin-footer" style="text-align:center; padding:12px 0; font-size:14px; color:#666; line-height:1.2;">
+    <!-- 第一行：隐私政策、免责声明、站点地图、Robots协议 -->
     <div style="margin:0;">
-        <a href="<?php $this->options->siteUrl(); ?>privacy.html">隐私政策</a>
+    <a href="/privacy">隐私政策</a> |
         &nbsp;|&nbsp;
-        <a href="<?php $this->options->siteUrl(); ?>disclaimer.html">免责声明</a>
+    <a href="/statement">免责声明</a> |
         &nbsp;|&nbsp;
-        <a href="<?php $this->options->siteUrl(); ?>sitemap.xml">站点地图</a>
+    <a href="/sitemap.xml">站点地图</a> |
         &nbsp;|&nbsp;
-        <a href="<?php $this->options->siteUrl(); ?>robots.txt">Robots协议</a>
+    <a href="/sitemap.xml">>Robots协议</a>
     </div>
 
-    <!-- 第二行：版权信息，margin极小 -->
-    <div style="margin:3px 0;">
+    <!-- 第二行：版权 + Cloudflare防御 -->
+    <div style="margin:2px 0;">
         Copyright 2026 ©版权所有 
         <a href="<?php $this->options->siteUrl(); ?>" id="bg-link" target="_blank"><?php $this->options->title(); ?></a>
-    </div>
-
-    <!-- 第三行：Cloudflare + 运行时长 + 访问量 -->
-    <div style="margin:3px 0; font-size:12px;">
+        &nbsp;|&nbsp;
         <a href="https://dash.cloudflare.com/" class="active">
             <img src="https://img.shields.io/badge/Cloudflare加速防御-green?style=flat&logo=Claris" alt="Cloudflare" style="height:18px;vertical-align:middle;">
         </a>
-        &nbsp;&nbsp;
+    </div>
+
+    <!-- 第三行：运行时间 + 访问量 -->
+    <div style="margin:2px 0; font-size:12px;">
         <span style="color:#FFB448; font-weight:bold;">本站已安全运行:</span>
         <span id="momk"></span>
         &nbsp;&nbsp;
@@ -40,7 +40,7 @@
             timeold=(today.getTime()-BirthDay.getTime());
             daysold=Math.floor(timeold/(24*60*60*1000));
             hrsold=Math.floor((timeold%(24*60*60*1000))/(60*60*1000));
-            minsold=Math.floor((timeold%(60*60*1000))/(60*1000));
+            minsold=Math.floor((timeold%(60*1000))/(60*1000));
             seconds=Math.floor((timeold%(60*1000))/1000);
             momk.innerHTML=daysold+"天"+hrsold+"小时"+minsold+"分"+seconds+"秒";
         }
@@ -67,7 +67,6 @@
             75%{color:#66e616;}
             100% {color:#67bd31;}
         }
-        /* 底部链接统一浅灰 hover变色 */
         #rin-footer a {
             color:#666;
             text-decoration:none;
